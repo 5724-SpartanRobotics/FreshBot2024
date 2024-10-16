b63152418b091b4d34b65a3d7c8297c0743a16e2
rickshaw5724 <rick.william.shaw@gmail.com>
2024-10-16 18:19:42 -0400

    Changed to NEO for top secondary

--- src/main/java/frc/robot/Subsystems/Intake.java
+++ src/main/java/frc/robot/Subsystems/Intake.java
@@ -4,6 +4,9 @@ import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPXConfiguration;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkBase.IdleMode;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Subsystems.Constant.IntakeConstants;
@@ -11,17 +14,17 @@ import frc.robot.Subsystems.Constant.IntakeConstants;
 public class Intake extends SubsystemBase{
     private VictorSPX intakeTop = new VictorSPX(IntakeConstants.TopMotorCanId);
     private VictorSPX intakeBot = new VictorSPX(IntakeConstants.BottomBeltsMotorCanId);
-    private VictorSPX topSecondary = new VictorSPX(IntakeConstants.TopSecondaryCanId);
+    private CANSparkMax topSecondary = new CANSparkMax(IntakeConstants.TopSecondaryCanId, MotorType.kBrushless);
     
     public Intake(){
         //restore all settings
         intakeTop.configAllSettings(new VictorSPXConfiguration());
         intakeBot.configAllSettings(new VictorSPXConfiguration());
-        topSecondary.configAllSettings(new VictorSPXConfiguration());
+        topSecondary.restoreFactoryDefaults();
 
         intakeTop.setNeutralMode(NeutralMode.Brake);
         intakeBot.setNeutralMode(NeutralMode.Brake);
-        topSecondary.setNeutralMode(NeutralMode.Brake);
+        topSecondary.setIdleMode(IdleMode.kBrake);
 
         intakeTop.setInverted(false);
         topSecondary.setInverted(false);
@@ -35,6 +38,6 @@ public class Intake extends SubsystemBase{
 
     public void RunTopIntake(double speed)
     {
-        topSecondary.set(VictorSPXControlMode.PercentOutput, speed);
+        topSecondary.set(speed);
     }
 }
