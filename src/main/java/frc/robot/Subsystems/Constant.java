70cbfcc354f514e4478fa2e31d6eb35850e9b774
dsjohann <dsjohann@mail.roanoke.edu>
2024-10-16 18:38:26 -0400

    can change

--- src/main/java/frc/robot/Subsystems/Constant.java
+++ src/main/java/frc/robot/Subsystems/Constant.java
@@ -51,7 +51,7 @@ public class Constant {
     public static final class IntakeConstants{
         public static final int TopMotorCanId = 38;
         public static final int BottomBeltsMotorCanId = 15;
-        public static final int TopSecondaryCanId = 1;
+        public static final int TopSecondaryCanId = 18;
         public static final double TopIntakePercentSpeed = 1.0;
         public static final double BotIntakePercentSpeed = 0.8;
         public static final double TopSecondaryPercentSpeedMult = 0.5;
